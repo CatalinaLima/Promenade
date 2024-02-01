1d41d2190d09524f1d184165208980d05d89b098
Catalina Lima Brun <catalinalbrun@gmail.com>
2024-01-31 19:00:50 -0700

    studio

--- src/components/pages/Estudio.jsx
+++ src/components/pages/Estudio.jsx
@@ -13,15 +13,15 @@ const Estudio = () => {
         <img className="encabezado" src="/assets/5_.jpg" alt="Proyecto" />
       </div>
       <div>
-        <div className= 'tab-content'>
+        <div className= 'tab-content-estudio'>
             <div className="part1">
                 <h3 className="spt" >PROMENADE</h3>
                 <p className="sp1">
-PROMENADE arquitectos es una firma de arquitectura e interiorismo con sede en Uruguay, especializada en proyectos residenciales, comerciales y gastronómicos de alto nivel. Buscamos una simbiosis perfecta entre el cliente, su público objetivo y el resultado final. Nuestra búsqueda es proyectar ambientes que evocan emociones mediante una arquitectura actual, divertida, sincera, que mantiene el equilibrio entre la funcionalidad, el diseño y la innovación. Nos especializamos en el diseño a medida, valorando el producto finamente acabado.</p>
+                PROMENADE arquitectos es una firma de arquitectura e interiorismo con sede en Uruguay, especializada en proyectos residenciales, comerciales y gastronómicos de alto nivel. Buscamos una simbiosis perfecta entre el cliente, su público objetivo y el resultado final. Nuestra búsqueda es proyectar ambientes que evocan emociones mediante una arquitectura actual, divertida, sincera, que mantiene el equilibrio entre la funcionalidad, el diseño y la innovación. Nos especializamos en el diseño a medida, valorando el producto finamente acabado.</p>
             </div>
-            <p className="sp2">Fundado en 2020, Promenade Arquitectos es un estudio con sede en Uruguay que trabaja con el diseño para enriquecer la experiencia humana. Como arquitectos y diseñadores multidisciplinarios, consideramos nuestro trabajo como un camino al bienestar, mejorando y optimizando cada espacio.Cada proyecto, ya sea arquitectura o interiorismo ejemplifica el minimalismo infundido con un objetivo más amplio de volver a sensibilizar el entorno construido a través del diseño. Nuestro objetivo es encontrar la esencia del diseño sin importar su escala o ubicación para crear arquitectura que se conecte con el deseo humano de pertenencia, autonomía e identidad.
-
-</p>
+            <div className="part1">
+            <p className="sp2">Fundado en 2020, Promenade Arquitectos es un estudio con sede en Uruguay que trabaja con el diseño para enriquecer la experiencia humana. Como arquitectos y diseñadores multidisciplinarios, consideramos nuestro trabajo como un camino al bienestar, mejorando y optimizando cada espacio.Cada proyecto, ya sea arquitectura o interiorismo ejemplifica el minimalismo infundido con un objetivo más amplio de volver a sensibilizar el entorno construido a través del diseño. Nuestro objetivo es encontrar la esencia del diseño sin importar su escala o ubicación para crear arquitectura que se conecte con el deseo humano de pertenencia, autonomía e identidad.</p>
+            </div>
         </div>
       </div>
       {/* Línea separativa */}
